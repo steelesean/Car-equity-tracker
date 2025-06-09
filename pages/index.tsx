6265fff4341b1e9e80a7fb20a83e1a39c5d23bef
steelesean <sean_mcharg@hotmail.co.uk>
2025-06-09 21:11:20 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../utils/supabase';
+import { supabase } from '../lib/supabaseClient';
 
 export default function Home() {
   const [email, setEmail] = useState('');
